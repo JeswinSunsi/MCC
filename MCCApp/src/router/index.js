871f43ab250615ab0586cbca51375d7d1c07335f
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-02 19:08:11 +0530

    Rename routes

--- MCCApp/src/router/index.js
+++ MCCApp/src/router/index.js
@@ -6,8 +6,8 @@ import QuizScreen from '../components/QuizScreen.vue'
 import ResultScreen from '../components/ResultScreen.vue'
 
 const routes = [
-  { path: '/', component: HomeScreen },
-  { path: '/intro', component: IntroScreen },
+  { path: '/home', component: HomeScreen },
+  { path: '/', component: IntroScreen },
   { path: '/quiz/:subject/:chaptername/:chapternum', component: QuizScreen },
   { path: '/result/:figure/:stars', component: ResultScreen },
 ]
