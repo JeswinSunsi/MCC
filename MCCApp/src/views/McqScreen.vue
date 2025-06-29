6fa551edda219a4ef516f002be48d3ba2d89d42e
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-06-29 11:19:50 +0530

    Add randomizer to MCQ

--- MCCApp/src/views/McqScreen.vue
+++ MCCApp/src/views/McqScreen.vue
@@ -117,10 +117,30 @@ function selectOption(index, correctness) {
   }
 }
 
+function shuffleArray(array) {
+  const shuffled = [...array]
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+  }
+  return shuffled
+}
+
+function randomizeQuizOptions(quizData) {
+  return quizData.map(question => ({
+    ...question,
+    options: shuffleArray(question.options)
+  }))
+}
+
 const loadSubjectData = async () => {
   try {
     const response = await fetch(`/questions/${subject}/${chapter}/MCQ.json`)
-    quizData.value = await response.json()
+    const rawQuizData = await response.json()
+    
+    // Randomize the options for each question
+    quizData.value = randomizeQuizOptions(rawQuizData)
+    
     totalQns.value = quizData.value.length
     oneBarProgress = 100 / totalQns.value
   } catch (error) {
